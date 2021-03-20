2114dc27f8fdf30e775f9f8afe6105b29d0b4a42
Oleg Chulkov <pilot114@bk.ru>
2021-03-21 01:55:00 +0700

    init

---
+++ _docker/units/Test.py
@@ -0,0 +1,5 @@
+import cv2
+
+class Test:
+    def run(Unit, params):
+        print(cv2.__version__)
---
+++ _docker/units/Unit.py
@@ -0,0 +1,7 @@
+class Unit:
+    def __init__(self):
+        # set depends and external links
+        print("Unit create")
+
+    def run(self, params):
+        print("Unit run!")
